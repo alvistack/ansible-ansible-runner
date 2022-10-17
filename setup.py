81a91b55f0e28e91a6634ca2da89755495e782f4
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2022-10-17 13:16:30 +0800

    alvistack/2.1.2
        git clean -xdf
        tar zcvf ../python-ansible-runner_2.1.2.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-ansible-runner.spec ../python-ansible-runner_2.1.2-1.spec
        mv ../python*-ansible-runner*2.1.2*.{gz,xz,spec,dsc} /osc/home\:alvistack/ansible-ansible-runner-2.1.2/
        rm -rf ../python*-ansible-runner*2.1.2*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    name="ansible-runner",
+    version="2.1.2",
+    author='Red Hat Ansible',
+    url="https://github.com/ansible/ansible-runner",
+    license='Apache',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'psutil',
+        'pexpect>=4.5',
+        'python-daemon',
+        'PyYAML',
+        'six',
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'ansible-runner = ansible_runner.__main__:main'
+        ]
+    }
 )
